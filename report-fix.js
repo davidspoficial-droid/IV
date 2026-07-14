057e59aad6aa468c59ae3069c54979e5fac245fd
davidspoficial-droid <david.spoficial@gmail.com>
2026-07-14 20:25:00 -0300

    chore: renova acabamento dos livros e semanas

--- report-fix.js
+++ report-fix.js
@@ -19,7 +19,7 @@
     return webButton && webButton.classList.contains('active') ? 'web' : reportVersion;
   };
 
-  import('./iv-report-postprocess-v5.js?v=20260714-1')
+  import('./iv-report-postprocess-v5.js?v=20260714-2')
     .then(function(){ return import('./iv-report-general-authority-v4.js?v=20260714-1'); })
     .then(function(){
       window.IVGenerateGeneralViewV4 = window.IVGenerateGeneralView;
