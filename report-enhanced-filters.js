17c3494fa11bde29bcb1ee719af347aff3ad7732
davidspoficial-droid <david.spoficial@gmail.com>
2026-07-14 17:56:08 -0300

    chore: publica revisão por equipe e faltas no relatório

--- report-enhanced-filters.js
+++ report-enhanced-filters.js
@@ -1,4 +1,4 @@
-import('./iv-report-general-authority.js?v=20260714-1')
+import('./iv-report-general-authority.js?v=20260714-2')
   .then(function(){ return import('./iv-revision-core.js?v=20260714-2'); })
   .then(function(){ return import('./iv-reports-generator-v2.js?v=20260714-1'); })
   .then(function(){ return import('./iv-reports-general-view-v3.js?v=20260714-1'); })
